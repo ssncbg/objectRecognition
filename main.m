e8a717cce84409863db269ed6e5d812eaa706a91
Susana <susana-catarina96@hotmail.com>
2019-04-22 10:54:34 +0100

    individual objects

--- main.m
+++ main.m
@@ -22,15 +22,13 @@ imshow(redThresholdImage);
 title('Threshold Image');
 
 [lb num]=bwlabel(redThresholdImage);
-regionProps = regionprops(lb,'area','FilledImage','Centroid');
+regionProps = regionprops(lb, rgb2gray(originalImage),'all');
 inds = find([regionProps.Area]>20);
 
 subplot(3, 3, 3);
 whiteImage = uint8(255 * ones(size(originalImage)));
 whiteImage = insertText(whiteImage,[10 10], sprintf('Number of objects: %d',length(inds)),'BoxOpacity',0, 'fontsize',80);
 imshow(whiteImage);
-%emptyImage.text(10, 10, 'Total objects:');
-
 
 subplot(3, 3, 4);
 imshow(redThresholdImage);
@@ -42,5 +40,18 @@ for i=1:length(inds)
     plot(centroid(1), centroid(2), 'r+', 'MarkerSize', 8, 'LineWidth', 1);
     hold off
 end
+
+figure;	% Create a new figure window.
+	% Maximize the figure window.
+	set(gcf, 'Units','Normalized','OuterPosition',[0 0 1 1]);
+	for k = 1 : length(inds)           % Loop through all blobs.
+		% Find the bounding box of each blob.
+		thisBlobsBoundingBox = regionProps(k).BoundingBox;  % Get list of pixels in current blob.
+		% Extract out this coin into it's own image.
+		subImage = imcrop(originalImage, thisBlobsBoundingBox);
+        
+        subplot(3, 4, k);
+		imshow(subImage);
+	end
 
 
