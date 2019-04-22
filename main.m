b8bf1eff8c0805ee6266fad1669550b216dcc87e
Susana <susana-catarina96@hotmail.com>
2019-04-22 15:37:59 +0100

    small changes

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 clear all, close all;
 set(gcf, 'units','normalized','outerposition',[0 0 1 1]);
 subplot(3, 3, 1);
-originalImage = imread('Moedas3.jpg');
+originalImage = imread('Moedas4.jpg');
 imshow(originalImage);
 title('Original Image');
 
@@ -11,8 +11,6 @@ redThresholdValue = graythresh(redBand);
 %uint8
 redThresholdValue = uint8(redThresholdValue * 255);
 redThresholdImage = (redBand >= redThresholdValue);
-%fills holes.
-%filledImage = imfill(redThresholdImage, 8,'holes');
 
 %se = strel('disk', 10);
 %filledImage = imerode(filledImage,se);
@@ -26,6 +24,7 @@ filledImage = imfill(redThresholdImage, 8,'holes');
 [lb num]=bwlabel(filledImage);
 regionProps = regionprops(lb, rgb2gray(originalImage),'all');
 
+%Fills small holes
 for i = 1 : length(regionProps)
     diferenceAreas = regionProps(i).Area - regionPropsBeforeFill(i).Area;
     if(diferenceAreas > regionProps(i).Area * 0.1)
@@ -55,19 +54,19 @@ end
 %Individual objects with perimeter and area
 figure;
 set(gcf, 'units','normalized','outerposition',[0 0 1 1]);
-	% Maximize the figure window.
-	for k = 1 : length(numberOfObjects)           % Loop through all blobs.
-		% Find the bounding box of each blob.
-		thisBlobsBoundingBox = regionProps(k).BoundingBox;  % Get list of pixels in current blob.
-		% Extract out this coin into it's own image.
-		subImage = imcrop(originalImage, thisBlobsBoundingBox);
-        
-        perimeter = regionProps(k).Perimeter;
-        area = regionProps(k).Area;
-        subplot(3, 4, k);
-		imshow(subImage);
-        title (sprintf('Perimeter: %g \nArea: %g', perimeter, area));
-    end
+% Maximize the figure window.
+for k = 1 : length(numberOfObjects)           % Loop through all blobs.
+    % Find the bounding box of each blob.
+    thisBlobsBoundingBox = regionProps(k).BoundingBox;  % Get list of pixels in current blob.
+    % Extract out this coin into it's own image.
+    subImage = imcrop(originalImage, thisBlobsBoundingBox);
+
+    perimeter = regionProps(k).Perimeter;
+    area = regionProps(k).Area;
+    subplot(3, 4, k);
+    imshow(subImage);
+    title (sprintf('Perimeter: %g \nArea: %g', perimeter, area));
+end
     
         
   
