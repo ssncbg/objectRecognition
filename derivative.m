d83eb6dbdfdc760779c5b5d343968a6a1b3f2d0c
Unknown <dianamlopes22@gmail.com>
2019-05-02 22:21:23 +0100

    1st derivative correct, 2nd not sure

--- derivative.m
+++ derivative.m
@@ -2,7 +2,7 @@ function derivative(image, numberOfObjects)
     figure;
     set(gcf, 'units','normalized','outerposition',[0 0 1 1]);
     plotPosition = 1;
-
+    stepSize=30;
     B = bwboundaries(image, 'noholes');
     %get 1st object to test
     for i=1:size(B)
@@ -10,37 +10,36 @@ function derivative(image, numberOfObjects)
         %convert to mat
         C = cell2mat(A);
         [row,col] = size(C);
-
         %split in X and Y
         X = [];
         Y = [];
+        X = [X, C(1,1)];
+        Y = [Y, C(1,2)];
         %get X and Y points 
-        countX = 0;
-        for i=1:row-2
+        for i=1:row-1
             T = [C(i+1,1), C(i+1,2); C(i,1), C(i,2)];        
-            if mod(i,10)==0
-                countX = countX+1;
-                X = [X; C(i,1)];
-                Y = [Y; C(i,2)];
+            if mod(i,stepSize) == 0
+                X = [X, C(i,1)];
+                Y = [Y, C(i,2)];
+            end
+            if i == row-1
+                X = [X, C(i+1,1)];
+                Y = [Y, C(i+1,2)];
+                X = [X, C(1,1)];
+                Y = [Y, C(1,2)];
             end
         end    
-        D = [];
-        for i=1:countX-1
-           if X(i+1)-X(i) == 0
-               D = [D;0];
-           else
-               D = [D;(Y(i+1)-Y(i))/(X(i+1)-X(i))];
-           end
-        end
+        D = diff(Y)/stepSize;
         subplot(numberOfObjects, 3, plotPosition);
-        plot(C(:, 1), C(:, 2));  
-        plotPosition = plotPosition + 1;
-        
-        subplot(numberOfObjects, 3, plotPosition);
-        plot(C(:,1));  
+        plot(X(:),Y(:));
         plotPosition = plotPosition + 1;
         
         subplot(numberOfObjects, 3, plotPosition);
         plot(D);  
+        plotPosition = plotPosition + 1;
+        
+        subplot(numberOfObjects, 3, plotPosition);
+        
+        plot(diff(D)/stepSize);  
         plotPosition = plotPosition + 1;
     end
